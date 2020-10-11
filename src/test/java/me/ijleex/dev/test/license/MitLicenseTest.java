1384f6ad5bec23c2997c5b10a47918b36f2ce75a
liym@home <ijliym@163.com>
2020-10-11 16:59:01 +0800

    修改 me.ijleex 包的范围及其许可证通知（License）
    src:me.ijleex..*||test:me.ijleex..*||lib:me.ijleex..*

--- src/test/java/me/ijleex/dev/test/license/MitLicenseTest.java
+++ src/test/java/me/ijleex/dev/test/license/MitLicenseTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2011-2018 ijym-lee
+ * Copyright (c) 2011-2020 mit-lic-users
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
