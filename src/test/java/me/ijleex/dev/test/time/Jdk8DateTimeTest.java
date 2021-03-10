7ceffb4a9b01e82cdeb250d1efd01a66819ba3d8
liym@com <ijliym@163.com>
2021-03-10 09:07:10 +0800

    修改

--- src/test/java/me/ijleex/dev/test/time/Jdk8DateTimeTest.java
+++ src/test/java/me/ijleex/dev/test/time/Jdk8DateTimeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 the original author or authors.
+ * Copyright 2011-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0.
  * See `LICENSE` in the project root for license information.
@@ -145,7 +145,7 @@ public class Jdk8DateTimeTest {
      * @since 2018-11-19 11:34
      */
     @Test
-    public void testCurrentTimeMillisToLocalTime() {
+    public void testTimeMillisToLocalTime() {
         long timeMillis = System.currentTimeMillis();
         Instant instant = Instant.ofEpochMilli(timeMillis);
         ZoneId zone = ZoneId.systemDefault();
