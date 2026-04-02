865ae99786312a40e719bf7405755e39032cd88c
ijliym <ijliym@163.com>
2026-04-02 18:17:52 +0800

    更新Git、Maven配置

--- docs/maven/clean-lastUpdated-files.bat
+++ docs/maven/clean-lastUpdated-files.bat
@@ -4,7 +4,6 @@ rem
 title Clean
 
 cd /d %~dp0
-for /r .\ %%f in (*.lastUpdated) do (
-  if exist %%f erase %%f
-)
+for /r repository\ %%f in (*.lastUpdated,*.repositories,*.sha1,*.sha256,*.sha512,*.properties,*.xml) do (if exist %%f erase %%f)
+for /r repository\ %%f in (unknown) do (if exist %%f rmdir /S /Q %%f)
 @pause
